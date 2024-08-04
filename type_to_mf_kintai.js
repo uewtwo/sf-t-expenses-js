78d2bdc055480b69c60c2b85a7a8e89da42ae1d6
uewtwo <nkyt161@gmail.com>
2024-08-05 08:25:03 +0900

    adapt mf

---
+++ type_to_mf_kintai.js
@@ -0,0 +1,25 @@
+async function enterAttendance() {
+  var attendances = []
+  var workdayRows = document.querySelectorAll(
+    "table.attendance-table-contents > tbody > tr.v3.attendance-table-row-error"
+  )
+  workdayRows.forEach((row) => {
+    attendances.push(
+      Array.from(row.querySelectorAll(".column-attendance-record")).map((r) =>
+        r.querySelector("input[type=text]")
+      )
+    )
+  })
+
+  attendances.forEach((attendance) => {
+    attendance[0].value = "10:0" + Math.floor(Math.random() * 10)
+    attendance[0].dispatchEvent(new Event("change", { bubbles: true }))
+  })
+  await new Promise((waiter) => setTimeout(waiter, 50))
+  attendances.forEach((attendance) => {
+    attendance[1].value = "19:1" + Math.floor(Math.random() * 10)
+    attendance[1].dispatchEvent(new Event("change", { bubbles: true }))
+  })
+}
+
+await enterAttendance()
